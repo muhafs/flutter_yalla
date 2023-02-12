512b4ffeee710d3a4f232d1b2d9acdf3b16451be
muhamadhaspin <muhamadhaspin@gmail.com>
2023-02-12 20:01:47 +0700

    - configs: install flutter project - configs: install google fonts package - configs: import assets from figma
    - features: create home page
    - features: create theme constants

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'hello',
+          style: GoogleFonts.poppins(
+            fontSize: 50,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF6C5ECF);
+const Color kSecondaryColor = Color(0xFF38ABBE);
+const Color kAlertColor = Color(0xFFED6363);
+const Color kPriceColor = Color(0xFF2C96F1);
+
+const Color kBgColor1 = Color(0xFF1F1D2B);
+const Color kBgColor2 = Color(0xFF2B2937);
+const Color kBgColor3 = Color(0xFF242231);
+
+const Color kPrimaryTextColor = Color(0xFFF1F0F2);
+const Color kSecondaryTextColor = Color(0xFF999999);
