081f9598deb7a0b5fa7ae7df10396f8d2495dfd5
muhamadhaspin <muhamadhaspin@gmail.com>
2023-02-15 20:53:41 +0700

    - features: create home page header [user welcome, avatar]

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yalla/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +11,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    return SafeArea(
+      child: ListView(
+        padding: const EdgeInsets.only(
+          top: kDefaultMargin,
+          left: kDefaultMargin,
+          right: kDefaultMargin,
+        ),
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello, Muhamad',
+                      style: kPrimaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    Text(
+                      '@muhamadhaspin',
+                      style: kSubTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const CircleAvatar(
+                radius: 27,
+                backgroundImage: AssetImage('assets/image_profile.png'),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
