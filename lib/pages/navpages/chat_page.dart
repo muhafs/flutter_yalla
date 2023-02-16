da0734de0630d704ef770ab7f6e8eeb6afeaf817
muhamadhaspin <muhamadhaspin@gmail.com>
2023-02-16 21:10:54 +0700

    - features: create chat empty page

--- lib/pages/navpages/chat_page.dart
+++ lib/pages/navpages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yalla/theme.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -10,8 +11,74 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Chat Page'),
+    return Column(
+      children: [
+        AppBar(
+          backgroundColor: kBgColor1,
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          title: Text(
+            'Message Support',
+            style: kPrimaryTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            color: kBgColor3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icon_headset.png',
+                  width: 80,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Opss no message yet?',
+                  style: kPrimaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  'You have never done a transaction',
+                  style: kSubTextStyle,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 44,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 24,
+                      ),
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      'Explore Store',
+                      style: kPrimaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
