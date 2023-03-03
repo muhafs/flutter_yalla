5ef09ccc2555943a000167436bb16ac4542b783c
muhamadhaspin <muhamadhaspin@gmail.com>
2023-03-03 05:32:37 -0800

    - features: slice wishlist page - features: fix chat page

--- lib/pages/navigation/chat_page.dart
+++ lib/pages/navigation/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yalla/components/chat/chat_tile.dart';
 import 'package:yalla/components/empty_page.dart';
+import 'package:yalla/components/page_app_bar.dart';
 import 'package:yalla/pages/chat/detail_chat_page.dart';
 import 'package:yalla/theme.dart';
 
@@ -13,7 +14,6 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   final EmptyPage emptyChat = const EmptyPage(
-    pageHeader: 'Message Support',
     imagePath: 'assets/icon_headset.png',
     subTitle: 'You have never done a transaction',
     title: 'Opss no message yet?',
@@ -22,28 +22,12 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    // return emptyChat;
     return Column(
       children: [
-        pageHeader(),
+        const PageAppBar('Message Support'),
         pageContent(),
+        // emptyChat,
       ],
-    );
-  }
-
-  Widget pageHeader() {
-    return AppBar(
-      backgroundColor: kBackground1,
-      centerTitle: true,
-      automaticallyImplyLeading: false,
-      elevation: 0,
-      title: Text(
-        'Message Support',
-        style: kTextStylePrimary.copyWith(
-          fontSize: 18,
-          fontWeight: medium,
-        ),
-      ),
     );
   }
 
@@ -52,30 +36,26 @@ class _ChatPageState extends State<ChatPage> {
       child: Container(
         width: double.infinity,
         color: kBackground3,
-        child: chatList(),
-      ),
-    );
-  }
-
-  Widget chatList() {
-    return ListView(
-      padding: const EdgeInsets.only(
-        left: kDefaultMargin,
-        top: 21,
-        right: kDefaultMargin,
-      ),
-      children: [
-        ChatTile(
-          onTap: () {
-            Navigator.pushNamed(context, DetailChatPage.id);
-          },
-          imageName: 'image_shop_logo',
-          title: 'Shoe Store',
-          lastChat:
-              'Good night, This item is on delivery state to your address',
-          date: 'Now',
+        child: ListView(
+          padding: const EdgeInsets.only(
+            left: kDefaultMargin,
+            top: 21,
+            right: kDefaultMargin,
+          ),
+          children: [
+            ChatTile(
+              onTap: () {
+                Navigator.pushNamed(context, DetailChatPage.id);
+              },
+              imageName: 'image_shop_logo',
+              title: 'Shoe Store',
+              lastChat:
+                  'Good night, This item is on delivery state to your address',
+              date: 'Now',
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/pages/navigation/wish_page.dart
+++ lib/pages/navigation/wish_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yalla/components/empty_page.dart';
+import 'package:yalla/components/page_app_bar.dart';
+import 'package:yalla/components/wish/wish_tile.dart';
 import 'package:yalla/theme.dart';
 
 class WishPage extends StatefulWidget {
@@ -9,12 +12,53 @@ class WishPage extends StatefulWidget {
 }
 
 class _WishPageState extends State<WishPage> {
+  final EmptyPage emptyWish = const EmptyPage(
+    imagePath: 'assets/image_wishlist.png',
+    title: 'You don\'t have dream shoes?',
+    subTitle: 'Let\'s find your favorite shoes',
+    buttonText: 'Explore Store',
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Wish Page',
-        style: kTextStylePrimary,
+    return Column(
+      children: [
+        const PageAppBar('Favorite Shoes'),
+        pageContent(),
+        // emptyWish,
+      ],
+    );
+  }
+
+  Widget pageContent() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        color: kBackground3,
+        child: ListView(
+          padding: const EdgeInsets.only(
+            left: kDefaultMargin,
+            top: 21,
+            right: kDefaultMargin,
+          ),
+          children: const [
+            WishTile(
+              imageName: 'image_shoes.png',
+              title: 'Predator 20.3 Firm Ground Boots',
+              price: '68.39',
+            ),
+            WishTile(
+              imageName: 'image_shoes.png',
+              title: 'Predator 20.3 Firm Ground Boots',
+              price: '68.39',
+            ),
+            WishTile(
+              imageName: 'image_shoes.png',
+              title: 'Predator 20.3 Firm Ground Boots',
+              price: '68.39',
+            ),
+          ],
+        ),
       ),
     );
   }
