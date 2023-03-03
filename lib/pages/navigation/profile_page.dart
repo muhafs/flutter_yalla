4ec9ad1c87affffb2ec8fb1578600697b100f3a8
muhamadhaspin <muhamadhaspin@gmail.com>
2023-03-03 05:47:50 -0800

    - features: create profile page header

--- lib/pages/navigation/profile_page.dart
+++ lib/pages/navigation/profile_page.dart
@@ -11,11 +11,51 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Page',
-        style: kTextStylePrimary,
-      ),
+    return Column(
+      children: [
+        AppBar(
+          backgroundColor: kBackground1,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(kDefaultMargin),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                    child: Image.asset('assets/image_profile.png'),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello Muhamad',
+                          style: kTextStylePrimary.copyWith(
+                            fontWeight: semibold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Text(
+                          '@muhamadhaspin',
+                          style: kTextStyleSub.copyWith(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/button_exit.png',
+                    width: 20,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
