9594d60e223a96d4be96fc927ac44224acb31c5f
muhamadhaspin <muhamadhaspin@gmail.com>
2023-03-20 19:29:18 +0700

    - features: create category model - features: create gallery model - features: create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,54 @@
+import 'package:yalla/models/category_model.dart';
+import 'package:yalla/models/gallery_model.dart';
+
+class ProductModel {
+  late int id;
+  late String name;
+  late double price;
+  late String description;
+  String? tags;
+  late CategoryModel category;
+  late DateTime createdAt;
+  late DateTime updatedAt;
+  late List<GalleryModel> galleries;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    this.tags,
+    required this.category,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.galleries,
+  });
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    price = double.parse(json['price'].toString());
+    description = json['description'];
+    tags = json['tags'];
+    category = CategoryModel.fromJson(json['category']);
+    createdAt = DateTime.parse(json['created_at']);
+    updatedAt = DateTime.parse(json['updated_at']);
+    galleries = json['galleries']
+        .map((gallery) => GalleryModel.fromJson(gallery))
+        .toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'description': description,
+      'tags': tags,
+      'category': category.toJson(),
+      'created_at': createdAt.toString(),
+      'updated_at': updatedAt.toString(),
+      'galleries': galleries.map((gallery) => gallery.toJson()).toList(),
+    };
+  }
+}
